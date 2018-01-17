b9611044bef483b7ea573fa90c077197d0102ef3
@Kirill <alias kirill_prahov@mail.ru>
2018-01-17 01:01:28 +0100

    Merge branch 'test1' of C:\Users\Kirill\Dropbox\workspaceMAC\wikiplag-multi-test1 with conflicts.

---
+++ plagiarismFinder/src/test/scala/de/htwberlin/f4/wikiplag/plagiarism/MatchTest.scala
@@ -0,0 +1,19 @@
+package de.htwberlin.f4.wikiplag.plagiarism
+
+import org.scalatest.FunSuite
+
+class MatchTest extends FunSuite {
+
+  test("testDocId") {
+
+  }
+
+  test("testPositon") {
+
+  }
+
+  test("testToString") {
+
+  }
+
+}
